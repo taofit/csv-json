b5c64f7e327ee613a0af695a3fe416d494bb5d11
Tao <tao.sun@zymego.com>
2022-09-25 21:13:25 +0200

    return some dummy json data from post request

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"html"
-	"net/http"
 	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 type hierarchyHandler struct{}
+type Data struct {
+	Item_id string `json:"item_id"`
+	Level_1 string `json:"level_1"`
+	Level_2 string `json:"level_2"`
+}
 
 func (f *hierarchyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
-	fmt.Println(csv.NewReader(r.Body).ReadAll())
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	records, err := csv.NewReader(r.Body).ReadAll()
+	if err != nil {
+		log.Fatalf("something wrong %v", err)
+	}
+	var allData = []Data{}
+	for _, v := range records {
+		allData = append(allData, Data{v[0], v[1], v[2]})
+	}
+	fmt.Println(allData)
+	json.NewEncoder(w).Encode(allData)
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", &hierarchyHandler{})
 	err := http.ListenAndServe(":8080", &hierarchyHandler{})
-	if (err != nil) {
+	if err != nil {
 		fmt.Printf("Error: %v", err.Error())
 	}
 }
